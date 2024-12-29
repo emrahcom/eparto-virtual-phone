87b0e90a7c39275bc0c7f634f4b4b58a16a3b09d
emrah <emrah.eryilmaz@nordeck.net>
2024-12-29 18:41:50 +0300

    feat(call): getting msg in the popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,13 @@
-document.getElementById("btn").addEventListener("click", () => {
-  alert("Button clicked!");
+document.getElementById("btn").addEventListener("click", async () => {
+  console.log("Button clicked!");
+
+  const qs = new URLSearchParams(globalThis.location.search);
+  const msgId = qs.get("id");
+
+  if (msgId) {
+    console.log(msgId);
+    console.log(await chrome.storage.session.get(`call-${msgId}`));
+  } else {
+    console.log("no msg");
+  }
 });
