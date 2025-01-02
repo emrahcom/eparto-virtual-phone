49042619629f0c366adc220d6cffdd02b715fa41
emrah <emrah.eryilmaz@nordeck.net>
2025-01-02 15:53:36 +0300

    feat(ui): initilize events

--- ui/phone.js
+++ ui/phone.js
@@ -32,5 +32,51 @@ async function watchCall() {
 }
 
 // -----------------------------------------------------------------------------
+// main
+// -----------------------------------------------------------------------------
+updateUi();
+
+// -----------------------------------------------------------------------------
 // updateUi
 // -----------------------------------------------------------------------------
+async function updateUi() {
+  try {
+    const storedItems = await chrome.storage.session.get(`call-${MSGID}`);
+    const call = storedItems[`call-${MSGID}`];
+    if (!call) throw "missing call object";
+
+    await console.log(call);
+  } catch (e) {
+    if (DEBUG) console.error(e);
+
+    globalThis.close();
+  }
+}
+
+// -----------------------------------------------------------------------------
+// reject
+// -----------------------------------------------------------------------------
+// deno-lint-ignore no-unused-vars
+async function reject() {
+  try {
+    await console.log("rejected");
+
+    globalThis.close();
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// accept
+// -----------------------------------------------------------------------------
+// deno-lint-ignore no-unused-vars
+async function accept() {
+  try {
+    await console.log("accepted");
+
+    globalThis.close();
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
