cf8849da9e81054bf89305f32afa2d20e6d29618
emrah <emrah.eryilmaz@nordeck.net>
2025-01-03 20:57:08 +0300

    feat(popup): fetch the contact list

--- ui/popup.js
+++ ui/popup.js
@@ -1,1 +1,55 @@
-console.log("default");
+// -----------------------------------------------------------------------------
+// Globals
+// -----------------------------------------------------------------------------
+const DEBUG = true;
+
+// -----------------------------------------------------------------------------
+// main
+// -----------------------------------------------------------------------------
+initialize();
+
+// -----------------------------------------------------------------------------
+// initialize
+// -----------------------------------------------------------------------------
+async function initialize() {
+  try {
+    const contacts = await getContactList();
+
+    console.log(contacts);
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// getContactList
+// -----------------------------------------------------------------------------
+async function getContactList() {
+  try {
+    const storedPrivateKeys = await chrome.storage.local.get("private-key");
+    const code = storedPrivateKeys["private-key"];
+    if (!code) throw "missing private key";
+
+    const storedBaseUrls = await chrome.storage.local.get("base-url");
+    const baseUrl = storedBaseUrls["base-url"];
+    if (!baseUrl) throw "missing base url";
+
+    const url = `${baseUrl}/api/pub/contact/list`;
+    const payload = {
+      code: code,
+    };
+
+    const res = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(payload),
+    });
+    if (!res.ok) throw "failed request";
+
+    return await res.json();
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
