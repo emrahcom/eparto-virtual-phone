91a4275b60260101db5ab038af6d3319b94c5597
emrah <emrah.eryilmaz@nordeck.net>
2025-01-07 20:59:43 +0300

    feat(outcall): handle ringing and its answer

--- background.js
+++ background.js
@@ -5,6 +5,11 @@ import { getByCode } from "./lib/common.js";
 
 const DEBUG = true;
 
+// Expire time in minute for incoming call.
+const INCALL_EXPIRE_TIME = 1;
+// Expire time in minute for outgoing call.
+const OUTCALL_EXPIRE_TIME = 0.5;
+
 // -----------------------------------------------------------------------------
 // Alarms
 // -----------------------------------------------------------------------------
@@ -34,6 +39,9 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
   } else if (alarm.name.startsWith("cleanup-outcall-")) {
     const callId = alarm.name.substr(16);
     cleanupOutCall(callId);
+  } else if (alarm.name.startsWith("ring-outcall-")) {
+    const callId = alarm.name.substr(13);
+    ringOutCall(callId);
   }
 });
 
@@ -168,7 +176,7 @@ function initializeInCall(msg) {
     if (isNaN(expiredAt)) throw "invalid expire time";
     if (Date.now() > expiredAt.getTime()) return;
 
-    // Create the popup and show it.
+    // Create the incoming call popup and show it.
     chrome.windows.create({
       url: chrome.runtime.getURL(
         `ui/in-${msg.message_type}.html?id=${msg.id}`,
@@ -188,10 +196,13 @@ function initializeInCall(msg) {
 // -----------------------------------------------------------------------------
 function triggerCleanupInCall(msgId) {
   try {
-    // Remove all objects related with this incoming call after 1 min. There is
-    // no problem if the browser is closed before this is done, because there
-    // are only session objects which will be removed anyway after the session.
-    chrome.alarms.create(`cleanup-incall-${msgId}`, { delayInMinutes: 1 });
+    // Remove all objects related with this incoming call after the expire time.
+    // There is no problem if the browser is closed before this is done, because
+    // there are only session objects which will be removed anyway after the
+    // session.
+    chrome.alarms.create(`cleanup-incall-${msgId}`, {
+      delayInMinutes: INCALL_EXPIRE_TIME,
+    });
   } catch (e) {
     if (DEBUG) console.error(e);
   }
@@ -212,6 +223,8 @@ async function cleanupInCall(msgId) {
 
 // -----------------------------------------------------------------------------
 // startOutCall
+//
+// ui/contact.js triggers this function when the user clicks the phone button.
 // -----------------------------------------------------------------------------
 async function startOutCall(call) {
   try {
@@ -221,7 +234,7 @@ async function startOutCall(call) {
 
     // Save id of the active call as contact value. So, it is possible to find
     // if there is an active call in the background for this contact. UI needs
-    // this value to handle the call status.
+    // this value to update the call status on UI.
     const item = {
       [`contact-${call.contact_id}`]: call.id,
     };
@@ -234,6 +247,7 @@ async function startOutCall(call) {
     await chrome.storage.session.set(callItem);
 
     // Trigger the ringing loop.
+    triggerOutCallRing(call.id);
   } catch (e) {
     if (DEBUG) console.error(e);
   }
@@ -244,13 +258,15 @@ async function startOutCall(call) {
 // -----------------------------------------------------------------------------
 function triggerCleanupOutCall(callId) {
   try {
-    // Remove all objects related with this outgoing call after 30 sec. This
-    // will also end the call if there is no answer yet.
+    // Remove all objects related with this outgoing call after the expire time.
+    // This will also end the call if there is no answer yet.
     //
     // There is no problem if the browser is closed before this is done,
     // because there are only session objects which will be removed anyway after
     // the session.
-    chrome.alarms.create(`cleanup-outcall-${callId}`, { delayInMinutes: 0.5 });
+    chrome.alarms.create(`cleanup-outcall-${callId}`, {
+      delayInMinutes: OUTCALL_EXPIRE_TIME,
+    });
   } catch (e) {
     if (DEBUG) console.error(e);
   }
@@ -263,23 +279,107 @@ async function cleanupOutCall(callId) {
   try {
     if (!callId) throw "missing call id";
 
-    // Get stored call object.
+    // Get stored call object. ringOutCall deletes the call object before the
+    // expire time if it is already answered by other peer.
     let storedItems = await chrome.storage.session.get(`outcall-${callId}`);
     const call = storedItems[`outcall-${callId}`];
-    const contactId = call.contact_id;
+    if (!call) return;
 
-    // Remove the outgoing call object.
+    // Remove the outgoing call object, expired.
     await chrome.storage.session.remove(`outcall-${callId}`);
+
+    // Send a notification to the callee about the missing call.
+    const payload = {
+      id: call.id,
+    };
+    await getByCode("/api/pub/intercom/del-with-notification", payload);
 
     // Reset the active call value if it keeps this call id. If the active call
     // for the related contact is not this call then dont reset the value. This
     // means that a new call is stated for this contact and will be handled in
     // another thread.
+    const contactId = call.contact_id;
     storedItems = await chrome.storage.session.get(`contact-${contactId}`);
     const activeCall = storedItems[`contact-${contactId}`];
 
     if (activeCall === callId) {
       await chrome.storage.session.remove(`contact-${contactId}`);
+    }
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// triggerOutCallRing
+// -----------------------------------------------------------------------------
+function triggerOutCallRing(callId) {
+  try {
+    chrome.alarms.create(`ring-outcall-${callId}`, { delayInMinutes: 0.02 });
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// ringOutCall
+// -----------------------------------------------------------------------------
+async function ringOutCall(callId) {
+  try {
+    if (!callId) throw "missing call id";
+
+    // Get stored call object. cleanupOutCall deletes the call object if it
+    // is expired. So, no needs to ring anymore ifthis is the case.
+    let storedItems = await chrome.storage.session.get(`outcall-${callId}`);
+    const call = storedItems[`outcall-${callId}`];
+    if (!call) return;
+
+    // Get the active call.
+    storedItems = await chrome.storage.session.get(
+      `contact-${call.contact_id}`,
+    );
+    const activeCall = storedItems[`contact-${call.contact_id}`];
+
+    // Stop ringing if this call is not active anymore. This happens when it is
+    // stopped on UI.
+    if (activeCall !== call.id) return;
+
+    // Ring and handle the ring status. The response contains the latest status
+    // depending on the answer of the other peer.
+    const payload = {
+      id: call.id,
+    };
+    const ring = await getByCode("/api/pub/intercom/call/ring", payload);
+
+    handleRingStatus(ring, callId);
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// handleRingStatus
+// -----------------------------------------------------------------------------
+async function handleRingStatus(ring, call) {
+  try {
+    // Ring again after a while if still no response from the peer.
+    if (ring.status === "none" || ring.status === "seen") {
+      chrome.alarms.create(`ring-outcall-${call.id}`, { delayInMinutes: 0.02 });
+      return;
+    }
+
+    // Delete the call objects if it is accepted or rejected by other peer.
+    await chrome.storage.session.remove(`outcall-${call.id}`);
+    await chrome.storage.session.remove(`contact-${call.contact_id}`);
+
+    const payload = {
+      id: call.id,
+    };
+    await getByCode("/api/pub/intercom/del", payload);
+
+    // Go to the meeting room if accepted.
+    if (ring.status === "accepted") {
+      globalThis.open(call.url, "_blank");
     }
   } catch (e) {
     if (DEBUG) console.error(e);
