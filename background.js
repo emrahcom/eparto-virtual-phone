236f6dd8ee02f8a96f997b6d0c7da27071043ee9
emrah <emrah.eryilmaz@nordeck.net>
2025-01-09 12:25:58 +0300

    refactor(background): update comments

--- background.js
+++ background.js
@@ -50,6 +50,7 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
 // -----------------------------------------------------------------------------
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.action === "start-outcall") {
+    // Contact page sends this message when the user clicks the phone button.
     startOutCall(msg);
   }
 });
@@ -57,12 +58,16 @@ chrome.runtime.onMessage.addListener((msg) => {
 // -----------------------------------------------------------------------------
 // initial
 // -----------------------------------------------------------------------------
+// This is the initial ping which will update the user presence on the
+// server-side. There is also an alarm which runs the ping function
+// periodically.
 ping();
 
 // -----------------------------------------------------------------------------
 // ping
 // -----------------------------------------------------------------------------
 async function ping() {
+  // Update the user presence on the server-side to inform contacts.
   return await getByKey("/api/pub/identity/ping/bykey");
 }
 
@@ -70,6 +75,8 @@ async function ping() {
 // getIntercomMessages
 // -----------------------------------------------------------------------------
 async function getIntercomMessages() {
+  // Poll intercom messages from the server. This function is triggered by an
+  // alarm periodically.
   return await getByKey("/api/pub/intercom/list/bykey");
 }
 
@@ -81,10 +88,13 @@ function messageHandler(messages) {
     if (!messages) throw "missing message list";
     if (!Array.isArray(messages)) throw "invalid structure for message list";
 
+    // If a message doesn't exist on the server-side anymore then remove its
+    // local copy. This happens when the message is dropped by its initiator.
     removeOldMessagesFromStorage(messages);
 
     if (!messages.length) return;
 
+    // Process messages depending on their types.
     for (const msg of messages) {
       if (msg?.message_type === "call") {
         callMessageHandler(msg);
@@ -106,11 +116,13 @@ async function removeOldMessagesFromStorage(messages) {
     const ids = messages.map((msg) => msg.id);
 
     // Trace stored messages and remove it if it is not in the id list. This
-    // means that this message is removed by its owner on the server-side.
+    // means that this message is removed by its initiator on the server-side.
     for (const key of await chrome.storage.session.getKeys()) {
+      // Skip it if not a call message.
       if (!key.startsWith("incall-")) continue;
 
       const msgId = key.substr("incall-".length);
+      // Skip it if still exist on the server-side.
       if (ids.includes(msgId)) continue;
 
       await chrome.storage.session.remove(`incall-${msgId}`);
@@ -246,7 +258,7 @@ async function startOutCall(call) {
     };
     await chrome.storage.session.set(callItem);
 
-    // Trigger the ringing loop.
+    // Trigger ring loop.
     triggerOutCallRing(call.id);
   } catch (e) {
     if (DEBUG) console.error(e);
@@ -280,7 +292,8 @@ async function cleanupOutCall(callId) {
     if (!callId) throw "missing call id";
 
     // Get stored call object. ringOutCall deletes the call object before the
-    // expire time if it is already answered by other peer.
+    // expire time if answered by other peer. So, there is no need to clean up
+    // in this case.
     let storedItems = await chrome.storage.session.get(`outcall-${callId}`);
     const call = storedItems[`outcall-${callId}`];
     if (!call) return;
@@ -288,7 +301,8 @@ async function cleanupOutCall(callId) {
     // Remove the outgoing call object, expired.
     await chrome.storage.session.remove(`outcall-${callId}`);
 
-    // Send a notification to the callee about the missing call.
+    // Send a notification to the callee about the missing call. Currently the
+    // callee is informed by an email about the missing call.
     const payload = {
       id: call.id,
     };
@@ -296,12 +310,15 @@ async function cleanupOutCall(callId) {
 
     // Reset the active call value if it keeps this call id. If the active call
     // for the related contact is not this call then dont reset the value. This
-    // means that a new call is stated for this contact and will be handled in
+    // means that a new call is started for this contact and will be handled in
     // another thread.
     const contactId = call.contact_id;
     storedItems = await chrome.storage.session.get(`contact-${contactId}`);
     const activeCall = storedItems[`contact-${contactId}`];
 
+    // Remove the contact object if this call is its active call. So, the
+    // contact status will be idle in this case. If a new call is started for
+    // the related contact then dont touch this contact object.
     if (activeCall === callId) {
       await chrome.storage.session.remove(`contact-${contactId}`);
     }
@@ -315,6 +332,7 @@ async function cleanupOutCall(callId) {
 // -----------------------------------------------------------------------------
 function triggerOutCallRing(callId) {
   try {
+    // Trigger ring loop.
     chrome.alarms.create(`ring-outcall-${callId}`, { delayInMinutes: 0.02 });
   } catch (e) {
     if (DEBUG) console.error(e);
@@ -341,7 +359,7 @@ async function ringOutCall(callId) {
     const activeCall = storedItems[`contact-${call.contact_id}`];
 
     // Stop ringing if this call is not active anymore. This happens when it is
-    // stopped on UI.
+    // stopped on UI by the user.
     if (activeCall !== call.id) return;
 
     // Ring and handle the ring status. The response contains the latest status
@@ -370,10 +388,12 @@ async function handleRingStatus(ring, callId) {
       return;
     }
 
-    // Delete the call objects if it is accepted or rejected by other peer.
+    // Delete the call objects if it is accepted or rejected by other peer. This
+    // also means that no needs to ring aymore.
     await chrome.storage.session.remove(`outcall-${call.id}`);
     await chrome.storage.session.remove(`contact-${call.contact_id}`);
 
+    // Remove the related intercom object on the server-side.
     const payload = {
       id: call.id,
     };
